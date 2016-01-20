fc9130bc1bee4c000edcedfaadc28e989582e765
carter115 <422819555@qq.com>
2016-01-20 15:43:02 +0800

    mail update

--- shiyan/mail/mail-t1.py
+++ shiyan/mail/mail-t1.py
@@ -8,7 +8,7 @@ import string
 # smtplib.SMTP.starttls() # 启用TLS安全传输模式
 # smtplib.SMTP.quit()     # 断开smtp服务器的连接
 
-HOST = "smtp.qq.com"
+HOST = "smtp.139.com"
 SUBJECT = "Test email from python"
 TO = "10000@qq.com"
 FROM = "13500000000@139.com"
@@ -19,10 +19,18 @@ BODY = string.join(("From: %s" % FROM,
                     "",
                     text),"\r\n"
                    )
+# print(BODY)
+"""
+From: 13500000000@139.com
+To: 10000@qq.com
+Subject: Test email from python
+
+Python rules them all!
+"""
 server = smtplib.SMTP()
 server.connect(HOST,25)
 server.starttls()
-server.login(FROM,"password")
+server.login(FROM,"mypassword")
 server.sendmail(FROM,TO,BODY)
 server.quit()
 
